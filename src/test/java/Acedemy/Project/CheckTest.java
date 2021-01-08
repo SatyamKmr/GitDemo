252eeb8cf32b57a60e7f558b0772168cbe7f65f9
satyamkmr <satshivshun@gmail.com>
2021-01-08 20:39:14 +0530

    This is commit from new download

--- src/test/java/Acedemy/Project/CheckTest.java
+++ src/test/java/Acedemy/Project/CheckTest.java
@@ -8,11 +8,13 @@ public class CheckTest {
 	public void init()
 	{
 		System.out.println("It is initialisation");
+		System.out.println("It is new method for Git");
 	}
 	
 	@Test
 	public void mains()
 	{
 		System.out.println("It is main Method");
+		System.out.println("It is method inside mains");
 	}
 }
